bb133b9b36b487c61d4c3c299b880e0bff7cfbde
heinzketchup2711 <91111233+heinzketchup2711@users.noreply.github.com>
2024-05-13 07:50:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,20 +8,32 @@ import TasksListScreen from './TasksListScreen';
 import AddTaskScreen from './AddTaskScreen';
 import EditTaskScreen from './EditTaskScreen';
 import doc_pic_screen from './doc_pic_screen';
+import getstarted from './getstarted';
+import splash from './splash';
+import CalanderScreen from './calendar';
 import { StyleSheet } from 'react-native';
-
+import Timer from './Timer';
+import AlarmList from './AlarmList';
+import Alarm from './Alarm';
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile">
+      <Stack.Navigator initialRouteName="splash">
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="TasksList" component={TasksListScreen} />
         <Stack.Screen name="DocScan" component={doc_pic_screen} />
+        <Stack.Screen name="Calander" component={CalanderScreen} />
+        <Stack.Screen name="Timer" component={Timer} />
+        <Stack.Screen name="Alarmlist" component={AlarmList} />
+        <Stack.Screen name="Alarm" component={Alarm} />
         <Stack.Screen name="AddTask" component={AddTaskScreen} />
         <Stack.Screen name="EditTask" component={EditTaskScreen} />
+        <Stack.Screen name="getstarted" component={getstarted} />
+        <Stack.Screen name="splash" component={splash} />
       </Stack.Navigator>
+     
     </NavigationContainer>
   );
 }
